d163231ae120197e19488f2455f784412f965e51
lbuttignol <leandrobuttignol@gmail.com>
2016-01-10 00:14:06 -0300

    [#110926358] ABM Rubro Finalizado

--- tejastore/src/main/java/tejastore/Rubro.java
+++ tejastore/src/main/java/tejastore/Rubro.java
@@ -1,9 +1,23 @@
 package tejastore;
 
+import javax.persistence.Basic;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+import org.datanucleus.util.NucleusLogger;
+
 public class Rubro {
 
+	@Id
+	@SequenceGenerator(name="RUBRO_CODIGO_SEQ", allocationSize=50)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="RUBRO_CODIGO_SEQ")
 	private long id;
 
+	@Basic
 	private String nombre;
 
 	public long getId() {
@@ -25,6 +39,63 @@ public class Rubro {
 	@Override
 	public String toString() {
 		return "Rubro [id=" + id + ", nombre=" + nombre + "]";
+	}
+	
+	public void alta_ABM(EntityManager em){
+		EntityTransaction tx = em.getTransaction();
+		try{
+			tx.begin();	
+			em.persist(this);
+			tx.commit();
+		}catch(Exception e)
+        {
+            NucleusLogger.GENERAL.error(">> Exception persisting data", e);
+            System.err.println("Error persisting data : " + e.getMessage());
+            return;
+        }finally{
+        	if (tx.isActive())
+            {
+                tx.rollback();
+            }
+        	
+        }
+  	}
+	
+	public void baja_ABM(EntityManager em){
+		EntityTransaction tx = em.getTransaction();
+		try{
+			tx.begin();
+			if(id != 0)
+				em.remove(this);
+			tx.commit();
+		}catch(Exception e)
+        {
+            NucleusLogger.GENERAL.error(">> Exception persisting data", e);
+            System.err.println("Error persisting data : " + e.getMessage());
+            return;
+        }finally{
+        	if (tx.isActive())
+                tx.rollback();
+        }
+	}
+	
+	public void modificacion_ABM (EntityManager em){
+		EntityTransaction tx = em.getTransaction();
+		try{
+			tx.begin();
+			if(id != 0)
+				em.persist(this);
+			tx.commit();
+		}catch(Exception e)
+        {
+            NucleusLogger.GENERAL.error(">> Exception persisting data", e);
+            System.err.println("Error persisting data : " + e.getMessage());
+            return;
+        }finally{
+        	if (tx.isActive())
+                tx.rollback();
+        }
+	
 	}
 
 }
